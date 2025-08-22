905226e1eb8fd78b15feccc5099facac071afd7f
Pengfei Zhu <zhupengfei910426@gmail.com>
2025-08-22 17:21:16 +1000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'gameLib',
+  webDir: 'www'
+};
+
+export default config;
